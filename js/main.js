aee38fc2c1a3b013ba7d26c43f497b9af08634a3
HenriLemos <henriqueturbon@gmail.com>
2023-08-29 16:38:10 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0;
+
+
+function loadPokemonItens(offset, limit) {
+
+    /* converter a lista de pokemons(objetos) em HTML */
+pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    /* transforma o pokemon em uma lista ordenada utilizando o MAP */
+    /* junta todos os elementos da lista e converte em string */        
+    const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                     <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+            </div>                
+        </li>
+    `)
+    .join('') 
+    pokemonList.innerHTML += newHtml
+})
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
